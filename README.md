a768b6346627c483e661b289828556ffca512bde
RottenFruits <s_ogawa@akarf.com>
2019-01-14 22:26:17 +0900

    updates

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ docker rm go_bandit
 
 docker run -v /Users/ogawashouhei/Documents/project/go_bandit/src:/go/src -it --name go_bandit go_bandit /bin/bash
 
+docker run -p 8080:8080 -v /Users/ogawashouhei/Documents/project/go_bandit/src:/go/src -it --name go_bandit go_bandit /bin/bash
+
 go run src/go_bandit/*.go
--- src/go_bandit/main.go
+++ src/go_bandit/main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"html/template"
+	"sync"
+	"path/filepath"
+
 )
 
-func foo() {
-	log.Print("Hello world from foo!")
+type templateHandler struct{
+	once sync.Once
+	filename string
+	templ *template.Template
 }
 
-func main() {
-	foo()
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	t.once.Do(func(){
+		t.templ =
+		template.Must(template.ParseFiles(filepath.Join("templates",
+		t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
+
+
+func test(){
 	var arms BernoulliArms
 	probs := [2]float64{0.4, 0.8}
 	for _, p := range probs {
@@ -23,5 +39,14 @@ func main() {
 	log.Print(a)
 	log.Print(b)
 	log.Print(c)
+}
 
+func main() {
+	//test()
+	
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img/"))))
+	http.Handle("/", &templateHandler{filename: "index.html"})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
