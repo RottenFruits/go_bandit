8359b4564fab052503c4b65651b4a5b5fde28ecb
RottenFruits <s_ogawa@akarf.com>
2019-01-14 23:14:50 +0900

    fmt

--- src/go_bandit/main.go
+++ src/go_bandit/main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
-	"html/template"
 	"sync"
 )
 
-type templateHandler struct{
-	once sync.Once
+type templateHandler struct {
+	once     sync.Once
 	filename string
-	templ *template.Template
+	templ    *template.Template
 }
 
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	t.once.Do(func(){
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
 		t.templ =
-		template.Must(template.ParseFiles(t.filename))
+			template.Must(template.ParseFiles(t.filename))
 	})
 	t.templ.Execute(w, nil)
 }
 
-
-
-func test(){
+func test() {
 	var arms BernoulliArms
 	probs := [2]float64{0.4, 0.8}
 	for _, p := range probs {
@@ -40,7 +38,7 @@ func test(){
 
 func main() {
 	//test()
-	
+
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources/"))))
 	http.Handle("/", &templateHandler{filename: "resources/templates/index.html"})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
