3006be4e04bfecbe54b69bdcc6b2b928276e4a3a
RottenFruits <s_ogawa@akarf.com>
2019-01-14 23:13:44 +0900

    change directory

---
+++ src/go_bandit/resources/js/index.js
@@ -0,0 +1,13 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
