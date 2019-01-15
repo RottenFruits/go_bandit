c954887e392d390ce6adfedcdc1122b5dc42bb9c
RottenFruits <s_ogawa@akarf.com>
2019-01-15 22:45:23 +0900

    receive json

--- src/go_bandit/main.go
+++ src/go_bandit/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
-	"fmt"
-	"encoding/json"
 )
 
 type banditResults struct {
-    a  []int `json:a`
-    b  []float64 `json:b`
-    c  []float64 `json:c`
+	a []int     `json:a`
+	b []float64 `json:b`
+	c []float64 `json:c`
 }
 
 type templateHandler struct {
@@ -45,14 +47,14 @@ func test(w http.ResponseWriter, r *http.Request) {
 	log.Print(res)
 	//log.Print(b)
 	/*
-	res2, err := json.Marshal(res)
-	if err != nil {
-		panic(err)
-	}
+		res2, err := json.Marshal(res)
+		if err != nil {
+			panic(err)
+		}
 	*/
 	d, _ := json.Marshal(res)
 	fmt.Fprintf(w, string(d))
-	
+
 	//w.Header().Set("Content-Type", "application/json")
 	//log.Print(res)
 	//fmt.Fprint(w, res2)
@@ -61,9 +63,43 @@ func test(w http.ResponseWriter, r *http.Request) {
 // リクエストを処理する関数
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World from Go.")
-	test(w, r)
-}
 
+	//Validate request
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if r.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//To allocate slice for request body
+	length, err := strconv.Atoi(r.Header.Get("Content-Length"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	//Read body data to parse json
+	body := make([]byte, length)
+	length, err = r.Body.Read(body)
+	if err != nil && err != io.EOF {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	//parse json
+	var jsonBody map[string]interface{}
+	err = json.Unmarshal(body[:length], &jsonBody)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Print(jsonBody)
+
+	//test(w, r)
+}
 
 func main() {
 	//test()
--- src/go_bandit/resources/js/index.js
+++ src/go_bandit/resources/js/index.js
@@ -11,32 +11,105 @@ var app_ = new Vue({
         selected:2,
         start_flag: false,
         treasures: [
-            {state: 0, key: 0, link:"/resources/images/kaizoku_takarabako.png"}, 
-            {state:0, key: 1, link:"/resources/images/kaizoku_takarabako.png"}
+            {state: 0, key: 0, link:"/resources/images/kaizoku_takarabako.png", visible:true}, 
+            {state:0, key: 1, link:"/resources/images/kaizoku_takarabako.png", visible:true},
+            {state:0, key: 2, link:"/resources/images/kaizoku_takarabako.png", visible:false},
+            {state:0, key: 3, link:"/resources/images/kaizoku_takarabako.png", visible:false},
+            {state:0, key: 4, link:"/resources/images/kaizoku_takarabako.png", visible:false}
         ],
         arm_probs: [
-            {prob: 0.3, key: 0}, 
-            {prob: 0.3, key: 1}
+            {prob: 0.3, key: 0, visible:true}, 
+            {prob: 0.3, key: 1, visible:true},
+            {prob: 0.3, key: 2, visible:false},
+            {prob: 0.3, key: 3, visible:false},
+            {prob: 0.3, key: 4, visible:false}
         ]
     },
     methods:{
         start:function(){
             this.start_flag = true
-            console.log(this.treasures)
-            this.treasures.splice(0, 1)
+            n_arms = Number(this.selected)
+            config = {
+                headers:{
+                  'X-Requested-With': 'XMLHttpRequest',
+                  'Content-Type':'application/json'
+                },
+                withCredentials:true,
+              }
+              
+              url = "http://localhost:8080/a"
+      
+              axios.post(url,{
+                  n_arms:n_arms, 
+                  arm_probs:this.arm_probs
+                },
+                config)
+              .then(function(res){
+                app.result = res.data
+                console.log(res)
+              })
+              .catch(function(res){
+      　　　　　　app.result = res.data
+                console.log(res)
+              })
+
             console.log("start")
         },
         stop:function(){
             this.start_flag = false
-            this.treasures.push({state:0, key:2, link:"/resources/images/kaizoku_takara.png"});
             console.log("stop")            
         },
         clear:function(){
             console.log("clear")            
         },
         number_of_arms_select:function(){
-            console.log(this.selected)
             this.selected = Number(this.selected)
+
+            if(this.selected == 2){
+                this.arm_probs[0]['visible'] = true
+                this.arm_probs[1]['visible'] = true
+                this.arm_probs[2]['visible'] = false
+                this.arm_probs[3]['visible'] = false
+                this.arm_probs[4]['visible'] = false
+                this.treasures[0]['visible'] = true
+                this.treasures[1]['visible'] = true
+                this.treasures[2]['visible'] = false
+                this.treasures[3]['visible'] = false
+                this.treasures[4]['visible'] = false
+            }else if(this.selected == 3){
+                this.arm_probs[0]['visible'] = true
+                this.arm_probs[1]['visible'] = true
+                this.arm_probs[2]['visible'] = true
+                this.arm_probs[3]['visible'] = false
+                this.arm_probs[4]['visible'] = false
+                this.treasures[0]['visible'] = true
+                this.treasures[1]['visible'] = true
+                this.treasures[2]['visible'] = true
+                this.treasures[3]['visible'] = false
+                this.treasures[4]['visible'] = false
+            }else if(this.selected == 4){
+                this.arm_probs[0]['visible'] = true
+                this.arm_probs[1]['visible'] = true
+                this.arm_probs[2]['visible'] = true
+                this.arm_probs[3]['visible'] = true
+                this.arm_probs[4]['visible'] = false
+                this.treasures[0]['visible'] = true
+                this.treasures[1]['visible'] = true
+                this.treasures[2]['visible'] = true
+                this.treasures[3]['visible'] = true
+                this.treasures[4]['visible'] = false
+            }else if(this.selected == 5){
+                this.arm_probs[0]['visible'] = true
+                this.arm_probs[1]['visible'] = true
+                this.arm_probs[2]['visible'] = true
+                this.arm_probs[3]['visible'] = true
+                this.arm_probs[4]['visible'] = true
+                this.treasures[0]['visible'] = true
+                this.treasures[1]['visible'] = true
+                this.treasures[2]['visible'] = true
+                this.treasures[3]['visible'] = true
+                this.treasures[4]['visible'] = true
+            }
         }
     }
 })
