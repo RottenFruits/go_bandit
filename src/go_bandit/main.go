22194d4d940a8c6b4f4d977ed580256dc48f4529
RottenFruits <s_ogawa@akarf.com>
2019-01-15 21:00:49 +0900

    updates

--- src/go_bandit/main.go
+++ src/go_bandit/main.go
@@ -5,7 +5,15 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"fmt"
+	"encoding/json"
 )
+
+type banditResults struct {
+    a  []int `json:a`
+    b  []float64 `json:b`
+    c  []float64 `json:c`
+}
 
 type templateHandler struct {
 	once     sync.Once
@@ -21,8 +29,10 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, nil)
 }
 
-func test() {
+func test(w http.ResponseWriter, r *http.Request) {
 	var arms BernoulliArms
+
+	//log.Print(r)
 	probs := [2]float64{0.4, 0.8}
 	for _, p := range probs {
 		arms = append(arms, BernoulliArm{p})
@@ -30,15 +40,35 @@ func test() {
 
 	bandit := Bandit{}
 	bandit.Initialize("EG", len(arms), 0.2)
-	a, b, c := bandit.test_algorithm(arms, 50, 500)
-	log.Print(a)
-	log.Print(b)
-	log.Print(c)
+	a, b, c := bandit.test_algorithm(arms, 10, 10)
+	var res = banditResults{a, b, c}
+	log.Print(res)
+	//log.Print(b)
+	/*
+	res2, err := json.Marshal(res)
+	if err != nil {
+		panic(err)
+	}
+	*/
+	d, _ := json.Marshal(res)
+	fmt.Fprintf(w, string(d))
+	
+	//w.Header().Set("Content-Type", "application/json")
+	//log.Print(res)
+	//fmt.Fprint(w, res2)
 }
+
+// リクエストを処理する関数
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World from Go.")
+	test(w, r)
+}
+
 
 func main() {
 	//test()
 
+	http.HandleFunc("/a", handler)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources/"))))
 	http.Handle("/", &templateHandler{filename: "resources/templates/index.html"})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- src/go_bandit/resources/js/index.js
+++ src/go_bandit/resources/js/index.js
@@ -1,13 +1,96 @@
-var app = new Vue({ 
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
-});
-
 var app6 = new Vue({
     el: '#app-6',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello!'
     }
 })
+
+Vue.component('arm-prob', {
+    data: function () {
+      return {
+        prob: 0.3,
+        key:null
+      }
+    },
+    template: '<input type="text" value=0.0 size="15"></input>'
+})
+
+Vue.component('treasure', {
+    data: function () {
+      return {
+        state: 0,
+        key:null
+      }
+    },
+    template: '<img src="/resources/images/kaizoku_takarabako.png" width="100"></img>'
+})
+
+
+var app_ = new Vue({ 
+    el: '#app_',
+    data:{
+        selected:2,
+        start_flag: false,
+        treasures: [
+            {state: 0, key: 0}, 
+            {state:0, key: 1}
+        ],
+        arm_probs: [
+            {prob: 0.3, key: 0}, 
+            {prob: 0.3, key: 1}
+        ]
+    },
+    methods:{
+        start:function(){
+            this.start_flag = true
+            console.log(this.treasures)
+            this.treasures.splice(0, 1)
+            console.log("start")
+        },
+        stop:function(){
+            this.start_flag = false
+            this.treasures.push({state:0, key:2});
+            console.log("stop")            
+        },
+        clear:function(){
+            console.log("clear")            
+        },
+        number_of_arms_select:function(){
+            console.log(this.selected)
+            this.selected = Number(this.selected)
+        }
+    }
+})
+
+
+var app = new Vue({
+    el:"#app",　
+    data:{　
+      url:"http://localhost:8080/a",
+      param:"{}",             
+      result:"...."  
+    },
+    methods:{　  
+      post: function(){
+        config = {
+          headers:{
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type':'application / x-www-form-urlencoded'
+          },
+          withCredentials:true,
+        }
+　　　　　 
+        param = JSON.parse(this.param)
+
+        axios.post(this.url,param,config)
+        .then(function(res){
+          app.result = res.data
+          console.log(res)
+        })
+        .catch(function(res){
+　　　　　　app.result = res.data
+          console.log(res)
+        })
+      }
+    }
+  })
